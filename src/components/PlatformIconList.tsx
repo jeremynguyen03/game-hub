9ba7abd868f8fdadc557ea527c3fb87c7a208750
Jeremy Nguyen <jeremynguyen03@gmail.com>
2023-08-18 20:48:11 +0700

    Display platform icon

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,41 @@
+import {
+  MdPhoneIphone,
+  SiNintendo,
+  BsGlobe,
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid
+} from "react-icons/all";
+import {Platform} from "../hooks/useGames";
+import {HStack, Icon} from "@chakra-ui/react";
+import {IconType} from "react-icons";
+
+interface Props {
+  platforms: Platform[]
+}
+
+const PlatformIconList = ({platforms}: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    nintendo: SiNintendo,
+    web: BsGlobe,
+  }
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color='gray.500'/>
+      ))}
+    </HStack>
+  )
+}
+
+export default PlatformIconList;
