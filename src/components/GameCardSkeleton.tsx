0c4c2517fee3817d85a835e5df978f4a0e4fdc11
Jeremy Nguyen <jeremynguyen03@gmail.com>
2023-08-19 12:13:15 +0700

    Display genres

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import {Card, CardBody, Skeleton, SkeletonText} from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width='256px'>
+    <Card>
       <Skeleton height='200px'/>
       <CardBody>
         <SkeletonText/>
